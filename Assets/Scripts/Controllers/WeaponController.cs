5299ed5f4e82a338d87e54ce2064fc98044c5329
Doron Kanaan <doronk@inceptionxr.com>
2022-04-18 16:25:57 +0300

    more

--- Assets/Scripts/Controllers/WeaponController.cs
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -5,42 +5,43 @@ namespace Controllers
 {
     public class WeaponController : MonoBehaviour {
 
-        private bool _isActive;
+        private bool _isFiring;
         private float _weaponSpeed;
         private Vector3 _playerPosition;
+        
         [SerializeField] private WeaponCollider weaponCollider;
 
         public void Init()
         {
-            _isActive = false;
             _weaponSpeed = GameController.Config.WeaponSpeed;
         }
 
         #region Weapon Usage handling
 
-        public async void HandleFire(Vector3 position)
-        {
-            _isActive = true;
-            _playerPosition = position;
-            StartFiring();
-            weaponCollider.Init(this);
-            while (_isActive)
-            {
-                DoProgress();
-                await Task.Yield();
-            }
-            transform.localScale = new Vector3(0f, 0f, 0f);
-        }
-        
         private void StartFiring()
         {
             transform.position = _playerPosition;
             transform.localScale = new Vector3(1f, 0f, 1f);
         }
         
-        private void DoProgress()
+        public async void HandleFire(Vector3 position)
         {
-            if (!_isActive) return;
+            _isFiring = true;
+            _playerPosition = position;
+            StartFiring();
+            weaponCollider.Init(this);
+            while (_isFiring)
+            {
+                Proceed();
+                await Task.Yield();
+            }
+            transform.localScale = new Vector3(0f, 0f, 0f);
+        }
+
+        private void Proceed()
+        {
+            if (!_isFiring) return;
+            
             transform.localScale += Vector3.up * (Time.deltaTime * _weaponSpeed);
         }
         
@@ -49,7 +50,8 @@ namespace Controllers
         //this method needs to be refactored to fire an event
         internal void HitSomething(Collider2D collision)
         {
-            _isActive = false;
+            _isFiring = false;
+            
             if (collision.CompareTag(GameController.Config.BallTagName))
             {
                 collision.GetComponent<IBallController>().PopBall();
